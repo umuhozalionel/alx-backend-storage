ffe9c19af09881f1f87a7b34bbea1c737b243414
umuhozalionel <muhozalionel@gmail.com>
2024-10-18 03:24:42 +0000

    initial commit

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Task: Create a table named users with specified attributes
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
