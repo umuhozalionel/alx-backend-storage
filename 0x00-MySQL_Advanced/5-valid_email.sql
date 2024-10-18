068ada914f5c6e9ccef56bfe2ef254df96e538c6
umuhozalionel <muhozalionel@gmail.com>
2024-10-18 04:00:00 +0000

    initial commit

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- Trigger to reset valid_email if the email has been changed
+DELIMITER //
+
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END //
+
+DELIMITER ;
+
