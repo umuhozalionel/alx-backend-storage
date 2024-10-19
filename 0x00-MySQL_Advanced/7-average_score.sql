4ca2c9bb5e557b6341dc740352df5e29a8f547e9
umuhozalionel <muhozalionel@gmail.com>
2024-10-19 08:01:02 +0000

    Initial comit

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,22 @@
+-- Create stored procedure ComputeAverageScoreForUser
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+
+    -- Compute the average score
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+
+    -- Store the average score in the users table
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END //
+
+DELIMITER ;
+
