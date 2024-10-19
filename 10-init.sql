90e6726abcfc828b362e6471bc3cd06a28f46d2c
umuhozalionel <muhozalionel@gmail.com>
2024-10-19 22:32:13 +0000

    Initial commit

---
+++ 10-init.sql
@@ -0,0 +1,15 @@
+-- Initial setup
+DROP TABLE IF EXISTS numbers;
+
+CREATE TABLE IF NOT EXISTS numbers (
+    a INT DEFAULT 0,
+    b INT DEFAULT 0
+);
+
+INSERT INTO numbers (a, b) VALUES (10, 2);
+INSERT INTO numbers (a, b) VALUES (4, 5);
+INSERT INTO numbers (a, b) VALUES (2, 3);
+INSERT INTO numbers (a, b) VALUES (6, 3);
+INSERT INTO numbers (a, b) VALUES (7, 0);
+INSERT INTO numbers (a, b) VALUES (6, 8);
+
