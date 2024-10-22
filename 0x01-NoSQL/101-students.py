16846322edb602e011d0f33d068c6c3bb8947c00
umuhozalionel <muhozalionel@gmail.com>
2024-10-22 16:08:38 -0700

    Initial commit

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" 101-students.py - Returns all students sorted by average score """
+
+def top_students(mongo_collection):
+    """ Returns all students sorted by average score """
+    pipeline = [
+        {
+            "$project": {
+                "name": 1,
+                "topics": 1,
+                "averageScore": { "$avg": "$topics.score" }
+            }
+        },
+        { "$sort": { "averageScore": -1 } }
+    ]
+    return list(mongo_collection.aggregate(pipeline))
